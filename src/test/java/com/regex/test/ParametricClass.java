8ef9330187a8472f408811b0b8f05240f6d70f5f
shiwani <shiwanipaunikar1@gmail.com>
2023-02-08 20:58:02 -0800

    Added:check parametarized test for multiple entry of email

---
+++ src/test/java/com/regex/test/ParametricClass.java
@@ -0,0 +1,40 @@
+package com.regex.test;
+
+import com.regex.assignments.RegExMain;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class ParametricClass {
+
+    private String email;
+    private RegExMain obj;
+
+    public ParametricClass(String email) {
+        this.email = email;
+    }
+
+    @Before
+    public void initialize() {
+        this.obj = new RegExMain();
+    }
+
+    @Parameters
+    public static Collection input() {
+        return Arrays.asList(new Object[] {"abc@yahoo.com", "abc-100@yahoo.com", "abc111@abc.com",
+                "abc.100@yahoo.com", "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com",
+                "abc+100@gmail.com"});
+    }
+
+    @Test
+    public void validateSampleEmailTest() {
+        Assert.assertTrue(obj.validEmail(email));
+    }
+
+}
--- src/test/java/com/regex/test/RegExTest2.java
+++ src/test/java/com/regex/test/RegExTest2.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-
 public class RegExTest2 {
     RegExMain obj = new RegExMain();
 
